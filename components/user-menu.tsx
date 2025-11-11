caaeda2238c57e05e3e907b66cd8bd1ec6329842
dawnpatrol04 <dawnpatrol04@gmail.com>
2025-11-11 14:57:38 -0800

    Fix: update user-menu import

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { logout } from "@/lib/appwrite";
+import { logout } from "@/lib/appwrite-client";
 import { useRouter } from "next/navigation";
 import { LogOut } from "lucide-react";
 
